89d15c09290bb97c75c57c9c86964cdcf11001ae
KonradKonradd2 <136739597+KonradKonradd2@users.noreply.github.com>
2023-06-22 21:34:05 +0200

    Update Department.java

--- java/Business/Department.java
+++ java/Business/Department.java
@@ -10,6 +10,9 @@ public class Department {
     }
 
     public void displayDepartmentInfo() {
+
+        /* wymienianie pracowników i od razu ich danych, a na dole oddzielenie ukośnikami */
+
         System.out.println("Department: " + name);
         System.out.println("Employees:");
         for (Employee employee : employees) {
