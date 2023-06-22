755654e956381df11d3b4d904237b036c5795301
KonradKonradd2 <136739597+KonradKonradd2@users.noreply.github.com>
2023-06-22 21:34:37 +0200

    Update Test.java

--- java/Business/Test/Test.java
+++ java/Business/Test/Test.java
@@ -5,6 +5,9 @@ import Business.Employee;
 
 public class Test {
         public static void main(String[] args) {
+
+            /* przykładowe dane do wypełnienia */
+
             Employee employee1 = new Employee("John Doe", 30, 5000);
             Employee employee2 = new Employee("Jane Smith", 35, 6000);
             Employee[] employees = {employee1, employee2};
